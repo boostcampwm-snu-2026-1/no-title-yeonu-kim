9abf85e29d5f8b819d4bdbd083600d1477940d3d
Yeonu-Kim <ywk0524@snu.ac.kr>
2026-05-29 23:42:18 +0900

    ♻️ path 상수화

---
+++ src/routes/path.ts
@@ -0,0 +1,5 @@
+export const PATH = {
+  LANDING: '/',
+  SIGN_IN: '/sign-in',
+  SIGN_UP: '/sign-up',
+};
--- src/routes/router-provider.tsx
+++ src/routes/router-provider.tsx
@@ -2,13 +2,14 @@ import { Route, Routes } from 'react-router';
 import { LandingPage } from '../pages/landing';
 import { SignInPage } from '../pages/sign-in';
 import { SignUpPage } from '../pages/sign-up';
+import { PATH } from './path';
 
 export const RouterProvider = () => {
   return (
     <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/sign-in" element={<SignInPage />} />
-      <Route path="/sign-up" element={<SignUpPage />} />
+      <Route path={PATH.LANDING} element={<LandingPage />} />
+      <Route path={PATH.SIGN_IN} element={<SignInPage />} />
+      <Route path={PATH.SIGN_UP} element={<SignUpPage />} />
     </Routes>
   );
 };
