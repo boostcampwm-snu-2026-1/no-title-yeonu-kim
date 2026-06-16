511e7bbaf093c75f017aabddda430d1314fccba8
Yeonu Kim <88171936+Yeonu-Kim@users.noreply.github.com>
2026-06-16 11:02:17 +0900

    🐛 presigned url 발급 시 토큰을 반드시 포함하도록 수정 (#47)

--- src/mocks/s3/schemas.ts
+++ src/mocks/s3/schemas.ts
@@ -3,6 +3,7 @@ export type S3FileType = 'REVIEW' | 'STORE';
 export type S3UploadRequest = {
   fileName: string;
   fileType: S3FileType;
+  contentType: string;
 };
 
 export type S3UploadResponse = {
